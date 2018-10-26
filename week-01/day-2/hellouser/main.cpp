815f3ccf7dcd0ffab13d099e9f493fdace0da69d
KisG93 <kisg93@gmail.com>
2018-10-26 17:21:24 +0200

    G

---
+++ week-01/day-2/hellouser/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+
+    std::string name;
+
+    std::cout << "What's your name?" << std::endl;
+    std::cin >> name;
+    std::cout << "Hi " << name << std::endl;
+
+    return 0;
+}
