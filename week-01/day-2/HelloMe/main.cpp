28c49572a3d3b7a998d8f40dbc4906d0b9ce9325
KisG93 <kisg93@gmail.com>
2018-10-26 13:18:17 +0200

    HelloMe

---
+++ week-01/day-2/HelloMe/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Csa Akos!" << std::endl;
+    std::cout << "Hellooo Mimy." << std:: endl;
+
+    
+    return 0;
+}
