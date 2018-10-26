2548333922a606e9f31d49c057f4d772da6b6277
KisG93 <kisg93@gmail.com>
2018-10-26 14:28:53 +0200

    G

---
+++ week-01/day-2/Swap/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int a = 123;
+    int b = 526;
+    int c = a;
+    a = b;
+    b = c;
+
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+    return 0;
+}
