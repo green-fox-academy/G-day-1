2338b48113f66351aeb0494cc630e8c0d93b8692
KisG93 <kisg93@gmail.com>
2018-10-26 14:22:38 +0200

    G

---
+++ week-01/day-2/CodingHours/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+
+    int daily = 6;
+    int semester = 85;
+    int weeklycodingh = 5 * 6;
+
+
+    std::cout << semester * daily << std::endl;
+    std::cout << float(52) / 102 << std::endl;
+
+    return 0;
+}
