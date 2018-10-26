a82b4f619c56dc27420af15e6e70cb348962cdd0
KisG93 <kisg93@gmail.com>
2018-10-26 13:40:16 +0200

    IntroMe

---
+++ week-01/day-2/IntroMe/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Gergo Barta" << std::endl;
+    std::cout << "44" << std:: endl;
+    std::cout << "1.75 m" << std::endl;
+    
+    return 0;
+}
