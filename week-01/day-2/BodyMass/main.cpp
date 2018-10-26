1be7d8c38561a79837b27e1fed3bd0aaa81f64d5
KisG93 <kisg93@gmail.com>
2018-10-26 14:36:20 +0200

    G

---
+++ week-01/day-2/BodyMass/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+
+    float kg = 81.2;
+    float m = 1.78;
+
+    std::cout << kg / (m * m) << std::endl;
+
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
