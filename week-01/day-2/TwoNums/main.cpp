b17020b53906409024c1783cb66a692f8dd94204
KisG93 <kisg93@gmail.com>
2018-10-26 13:49:38 +0200

    Nums

---
+++ week-01/day-2/TwoNums/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+
+    int a = 13;
+    int b = 22;
+
+
+    std::cout << a + b  << std::endl;
+    std::cout << a - b << std::endl;
+    std::cout << (float) a / b << std::endl;
+    std::cout << a * b << std::endl;
+
+
+
+
+    return 0;
+}
