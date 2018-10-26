f8e354942c06348f33a60bb0960be861a18b7c80
KisG93 <kisg93@gmail.com>
2018-10-26 17:07:30 +0200

    G

---
+++ week-01/day-2/Cuboid/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+
+
+
+    int a = 9;
+    int b = 9;
+    int c = 9;
+
+    int v = a * a * a;
+    int sa = a * a * 6;
+
+    std::cout << v << std::endl;
+    std::cout << sa << std::endl;
+    
+
+
+
+      return 0;
+}
