dd93ae412320dce5b127b4d175813fc60c283eaa
KisG93 <kisg93@gmail.com>
2018-10-28 20:15:14 +0100

    g

---
+++ week-01/day-2/animalslegs/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+
+    int chicken = 5;
+    int pig = 8;
+
+    std::cout << "All the animals have " << (chicken * 2) + (pig * 4) << " legs alltogether. :)" << std::endl;
+
+
+    return 0;
+}
