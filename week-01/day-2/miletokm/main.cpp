c7f2511115a3875b8cc7d049dfe0f4c0a8ce16c6
KisG93 <kisg93@gmail.com>
2018-10-28 20:03:22 +0100

    g

---
+++ week-01/day-2/miletokm/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+
+    int a = 126;
+
+
+    std::cout << "The distance is " << a << " kms, which is " << a * 0.62137119224  << " miles. :)" << std::endl;
+
+
+
+    return 0;
+}
