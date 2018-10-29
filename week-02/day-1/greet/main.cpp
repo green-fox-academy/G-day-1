85f67757da71f15bcf8608c39affb17432748afe
KisG93 <kisg93@gmail.com>
2018-10-29 14:18:47 +0100

    10.28.

---
+++ week-02/day-1/greet/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+
+std::string greet(std::string x);
+
+int main() {
+
+    std::string a1 = "Greenfox";
+    greet(a1);
+    
+
+    return 0;
+}
+
+std::string greet(std::string x) {
+std::cout << "Greetings dear , " << x << std::endl;
+
+
+}
