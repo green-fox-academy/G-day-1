b617617f9695a12d6bf3fce53d1828b53b5465e4
KisG93 <kisg93@gmail.com>
2018-10-30 17:03:03 +0100

    10.29.

--- week-02/day-2/solar/main.cpp
+++ week-02/day-2/solar/main.cpp
@@ -1,25 +1,22 @@
 #include <iostream>
-#include <string>
 #include <vector>
 
 std::vector<std::string> putSaturn(const std::vector<std::string>& planets)
 {
+    std::vector<std::string> temp = planets;
+    temp.push_back("Saturn");
+    return temp;
 
 }
 
-int main(int argc, char* args[])
-{
+int main() {
     std::vector<std::string> planets = {"Mercury","Venus","Earth","Mars","Jupiter","Uranus","Neptune"};
-
-    
 
     // Saturn is missing from the planetList
     // Insert it into the correct position
     // Create a method called putSaturn() which has list parameter and returns the correct list.
 
     // Expected output: Mercury Venus Earth Mars Jupiter Uranus Neptune Saturn
-
-
     for(const auto& planet : putSaturn(planets))
     {
         std::cout << planet << " ";
@@ -27,4 +24,3 @@ int main(int argc, char* args[])
 
     return 0;
 }
-
