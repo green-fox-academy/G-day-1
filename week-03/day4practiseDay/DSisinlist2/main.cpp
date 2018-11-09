1d29c68e975265c704e8ffb80160a9951ee44e4e
KisG93 <kisg93@gmail.com>
2018-11-09 13:26:54 +0100

    11.09.

--- week-03/day4practiseDay/DSisinlist2/main.cpp
+++ week-03/day4practiseDay/DSisinlist2/main.cpp
@@ -11,12 +11,12 @@ int main(int argc, char* args[])
     const std::vector<int> numbers = {2, 4, 6, 8, 10, 12, 14, 16};
     const std::vector<int> checker = {4, 8, 12, 16};
 
-
     // Check if vector contains all of the following elements: 4,8,12,16
     // Create a method that accepts vector as an input
     // it should return "true" if it contains all, otherwise "false"
 
 
+    // Expected output: "Noooooo"
     if(checkNums(numbers, checker)){
         std::cout << "Good :)" << std::endl;
     }
@@ -28,13 +28,11 @@ int main(int argc, char* args[])
 }
 
 bool checkNums(const std::vector<int> numbers, const std::vector<int> checker){
-    for(int i = 0; i < checker.size(); i++){
+    for(int i = 0; i < checker.size(); i++) {
+
         if ( std::find(numbers.begin(), numbers.end(), checker[i]) == numbers.end() ){
             return false;
         }
-    }
-
     return true;
-
-
+    }
 }
