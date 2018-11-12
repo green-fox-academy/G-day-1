9c8e9fd7d927acc42491a8fe303d6226478fe0a1
KisG93 <kisg93@gmail.com>
2018-11-12 14:53:46 +0100

    11.12.

---
+++ week-04/day1/WriteSingle/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main() {
+    std::ofstream myFile;
+    myFile.exceptions(std::ofstream::failbit | std::ofstream::badbit);
+    try {
+        myFile.open("../my-file.txt");
+        std::string content = "Barta Gergő";
+        myFile << content;
+        myFile.close();
+    } catch (std::ofstream::failure& e) {
+        std::cout << e.what() << std::endl;
+    }
+    return 0;
+
+}
