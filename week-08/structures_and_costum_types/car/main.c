f94bc1b243760fafcbbcf8dda7268aa8fe4e883c
KisG93 <kisg93@gmail.com>
2018-12-12 15:41:20 +0100

    12.12.

---
+++ week-08/structures_and_costum_types/car/main.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <string.h>
+
+enum car_type {
+    VOLVO,
+    TOYOTA,
+    LAND_ROVER,
+    TESLA};
+
+typedef struct car {
+    enum car_type type;
+    double km;
+    double gas;
+}car_t;
+
+const char* get_type_name(enum car_type car_type_type);
+void print_out_data(car_t x);
+
+int main()
+{
+    car_t volvo;
+    volvo.type = VOLVO;
+    volvo.km = 280;
+    volvo.gas = 100;
+
+    car_t toyota;
+    toyota.type = TOYOTA;
+    toyota.km = 300;
+    toyota.gas = 110;
+
+    car_t tesla;
+    tesla.type = TESLA;
+    tesla.km = 120;
+
+    print_out_data(volvo);
+    print_out_data(toyota);
+    print_out_data(tesla);
+
+    return 0;
+}
+
+void print_out_data(car_t x)
+{
+    if(x.type == TESLA) {
+        printf("The type of the car is %s, has %0.2lf km.\n", get_type_name(x.type), x.km);
+    }else{
+        printf("The type of the car is %s, has %0.2lf km, gets %0.2lf gas.\n", get_type_name(x.type), x.km, x.gas);
+    }
+}
+
+const char* get_type_name(enum car_type car_type_type) {
+    switch (car_type_type) {
+        case VOLVO:
+            return "Volvo";
+        case TOYOTA:
+            return "Toyota";
+        case TESLA:
+            return "Tesla";
+    }
+}
---
+++ week-08/structures_and_costum_types/coordinate/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+typedef struct coordinates{
+    
+};
+// Create a struct to store coordinates (for example: int x and int y)
+// Create an array which can store coordinates, do not create multidimensonal arrays here
+// Fill up the coordinates in the array with random number.
+
+int main()
+{
+
+    return 0;
+}
