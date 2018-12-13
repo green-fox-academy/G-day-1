b046f042274e04558856cba0211cd9e879130fc5
KisG93 <kisg93@gmail.com>
2018-12-13 13:48:39 +0100

    12.13.

--- week-08/structures_and_costum_types/house/main.c
+++ week-08/structures_and_costum_types/house/main.c
@@ -1,26 +1,64 @@
 #include <stdio.h>
+#include <string.h>
 
+typedef struct house {
+    char *address;
+    int price;
+    int room;
+    int area;
+    int market_price;
+}house_t;
 
-typedef struct house{
-
-
-};
+char worth_to_buy(house_t *x);
+int house_array(const int array[], int length);
 /*
-Create a struct that represents a House
-It should store:
- - The address of the house
- - The price in EUR
- - The number of rooms
- - The area of the house in square meters
-The market price of the houses is 400 EUR / square meters
-Create a function that takes a pointer to a house and decides if it's worth to buy
-(if the price is lower than the calculated price from it's area)
 Create a function that takes an array of houses (and it's length), and counts the
 houses that are worth to buy.
 */
 
 int main()
 {
+    house_t House1;
+    House1.address = "street_1";
+    House1.area = 100;
+    House1.price = 4;
+    House1.market_price = 400 * House1.area;
+
+    house_t House2;
+    House2.address = "street_2";
+    House2.area = 2;
+    House2.price = 100000000;
+    House2.market_price = 400 * House2.area;
+
+    house_t my_array[] = {House1, House2};
+
+    int xxx = sizeof(my_array)/ sizeof(my_array[0]);  //tömb méret
+    house_array(my_array, xxx);
+
 
     return 0;
+}
+
+char worth_to_buy(house_t *x)
+{
+    if(x->market_price > x->price){
+        printf("Worth it.\n");
+        return 1;
+    }else{
+        printf("Doesn't worth it.\n");
+        return 0;
+    }
+}
+
+int house_array(const int array[], int length)
+{
+    int count_worth = 0;
+
+    for (int i = 0; i < length; ++i) {
+        if(worth_to_buy(&array[i]) == 1){
+            count_worth++;
+        }
+    }
+    printf("%d", count_worth);
+    return count_worth;
 }
